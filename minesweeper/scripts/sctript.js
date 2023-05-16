35dbb4d751fbd00b01d02f62221ff3333fa873a7
Никита Корнеев <korneevsaya@MacBook-Air-Nikita.local>
2023-05-17 00:47:37 +0300

    feat: add basic handling of custom actions

--- minesweeper/scripts/sctript.js
+++ minesweeper/scripts/sctript.js
@@ -1,55 +1,73 @@
+
+
 function createField(size) {
     let field = [];
     for(let i = 0; i < size; i++){
-     field[i] = [];
+     const row = [];
      for(let j = 0; j< size; j++){
-         field[i][j] = 0;
+         row.push({isMine: false}) ;
      }
+     field.push(row);
     }
     return field;
- }
+}
  
- function createMines (field, numOfMines){
+function createMines (field, numOfMines){
      let placeMines = 0;
-     let row = Math.floor(Math.random() * field.length);
-     let col = Math.floor(Math.random() * field[0].length);
- 
      while (placeMines < numOfMines){
+        let row = Math.floor(Math.random() * field.length);
+        let col = Math.floor(Math.random() * field[0].length);
          if(!field[row][col].isMine) {
-             placeMines++;
-             field[row][col].isMine = true;
+            placeMines++;
+            field[row][col].isMine = true;
          }
-         row = Math.floor(Math.random() * field.length);
-         col = Math.floor(Math.random() * field[0].length);
      }
      return field;
- }
+}
  
  function addMarkupToPage (field){
      let markup = 
  `<div class="minesweeper_window">
  <div class="header"></div>
  <div class="field">`;
- 
      for(let i = 0; i < field.length; i++){
          for(let j = 0; j < field[i].length; j++){
-             markup += `<div class ="cell">1</div>`;
+            const currentCell = field[i][j];
+                markup += `<div class ="cell hidden data-mine=[${currentCell.isMine ? true : false}]"></div>`;
+            }   
          }
-     }
- 
      markup += `</div>
  <div class="bottom"></div>
  </div>`;
      document.write(markup);
  }
  
+function clearField(){
+    fieldElement.innerHTML = '';
+}
+
  let field = createField(10);
- console.log(field);
  field = createMines(field, 10);
+ console.log(field);
  addMarkupToPage(field);
  
- 
- 
+ const fieldElement = document.querySelector(".field");
+ fieldElement.addEventListener('click', event => {
+    const clickedCell = event.target;
+    const cellClasses = clickedCell.classList;
+    console.log(cellClasses);
+    
+    if(cellClasses.contains('field')) console.log('field!');
+    else if(cellClasses.contains('cell') && cellClasses.contains('hidden') && clickedCell.dataset.mine === false) {
+        cellClasses.remove('hidden');
+        cellClasses.add('opened');
+    }
+    else{
+        clearField();
+    }
+    console.log(cellClasses);
+
+ });
  
  
  
